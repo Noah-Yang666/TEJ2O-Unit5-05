23214029fb777f0791e814f27ae0423dfbd6a262
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-12 22:28:40 +0000

    kind of rewrote typescript code into MicroPython(needs more editing)

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,23 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Noah
+Created on: March 2026
+This module is a Micro:bit MicroPython program that tracks distance using sonar
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+# variables
+distanceToObject = 0
+
+# setting up
+display.show(Image.HAPPY)
+
+# find distance using sonar
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        distanceToObject = sonar.ping(
+            DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters
+        )
+    display.scroll(distanceToObject + " cm")
+    display.show(Image.HAPPY)
